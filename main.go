7fbf3eb8a09d5e88184fb86fa1c3fbb0f7284df4
Les Aker <me@lesaker.org>
2018-03-10 12:46:45 -0500

    put in initial logic

--- main.go
+++ main.go
@@ -1,26 +1,75 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/akerl/go-lambda/cloudwatch"
 	"github.com/akerl/go-lambda/s3"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"gopkg.in/yaml.v2"
 )
 
-// TODO: Add config file structure
-type configFile struct{}
-
 var config *configFile
 
-func handler(e events.SNSEvent) error {
-	log.Printf("%+v", e)
+type configFile struct {
+	Alarms map[string]alarmConfig `json:"alarms"`
+}
+
+func (c *configFile) lookup(name string) (alarmConfig, error) {
+	conf, ok := c.Alarms[name]
+	if ok {
+		return conf, nil
+	}
+	conf, ok = c.Alarms["default"]
+	if ok {
+		return conf, nil
+	}
+	return conf, fmt.Errorf("No config found: %s", name)
+}
+
+type alarmConfig struct {
+	URL string `json:"url"`
+}
+
+func (ac *alarmConfig) notify(m cloudwatch.AlarmMessage) error {
+	slack := slackMsg{Text: m.Subject}
+	buf, err := json.Marshal(slack)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(ac.URL, "application/json", bytes.NewReader(buf))
+	return err
+}
+
+type slackMsg struct {
+	Text string `json:"text"`
+}
+
+func handler(e cloudwatch.SNSEvent) error {
 	if config == nil {
 		return fmt.Errorf("Config failed to load")
 	}
+
+	for _, r := range e.Records {
+		m, err := r.DecodedMessage()
+		if err != nil {
+			return err
+		}
+		ac, err := config.lookup(m.AlarmName)
+		if err != nil {
+			return err
+		}
+		err = ac.notify(m)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
